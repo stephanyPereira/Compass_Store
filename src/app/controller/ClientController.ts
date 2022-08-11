d3beebb93f9870f2a5c40148f436626fd9bb4490
Stephany <80916986+stephanyPereira@users.noreply.github.com>
2022-08-11 16:08:56 -0300

    chore: removing try catch

--- src/app/controller/ClientController.ts
+++ src/app/controller/ClientController.ts
@@ -2,21 +2,17 @@ import ClientService from '../service/ClientService';
 
 class ClientController {
   async create(req, res) {
-    try {
-      const { name, cpf, birthday, email, password, cep, number } = req.body;
-      const result = await ClientService.create({
-        name,
-        cpf,
-        birthday,
-        email,
-        password,
-        cep,
-        number,
-      });
-      return res.status(201).json(result);
-    } catch (error) {
-      return res.status(500).json({ error });
-    }
+    const { name, cpf, birthday, email, password, cep, number } = req.body;
+    const result = await ClientService.create({
+      name,
+      cpf,
+      birthday,
+      email,
+      password,
+      cep,
+      number,
+    });
+    return res.status(201).json(result);
   }
 }
 
