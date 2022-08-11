6d84c17f96cd3b9e6596c9877d8ffb394911d2e9
Stephany <80916986+stephanyPereira@users.noreply.github.com>
2022-08-11 09:02:41 -0300

    feat: setting API default route

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import App from './app';
 const app = new App();
 
 app.init().listen(process.env.PORT || 3000, () => {
-  console.info(`App starting at http://localhost:${process.env.PORT || 3000}`);
+  console.info(
+    `App starting at http://localhost:${process.env.PORT || 3000}/api/v1`,
+  );
   console.info(`Envs: ${process.env.TARGET || 'local'}`);
 });
